dee3f26b9f81f0189a6cf1f1b43a3431e49f08f8
artur.adamovich <artur.adamovich@libertexgroup.com>
2021-04-02 21:53:49 +0300

    Added dynamic content

--- Hotel Booking - layout/js/modal.js
+++ Hotel Booking - layout/js/modal.js
@@ -3,11 +3,18 @@ const dialogCard = infoDialog.querySelector('.dialog');
 const infoDialogTriggers = document.querySelectorAll('[data-target="info-dialog"]');
 const infoDialogCloseTriggers = document.querySelectorAll('[data-target-close="info-dialog"]');
 
+const modalTitle = document.querySelector(".modal__title");
+const modalContent = document.querySelector(".modal__content");
+
 for (const closeTrigger of infoDialogCloseTriggers) {
     closeTrigger.addEventListener('click', () => {
         const targetElem = document.getElementById(closeTrigger.dataset.targetClose);
 
         targetElem.style.display = 'none';
+
+        // reset modal content
+        modalTitle.innerText = '';
+        modalContent.innerText = '';
 
         // enable scroll
         document.body.style.overflow = 'visible';
@@ -15,11 +22,25 @@ for (const closeTrigger of infoDialogCloseTriggers) {
 }
 
 window.addEventListener('click', event => {
-    const isDialogTriggerClicked = [...infoDialogTriggers].some(
-        dialogTrigger => dialogTrigger.contains(event.target)
-    )
+    let title = '';
+    let mainContent = '';
+    const classWrapper = ".card-type-wrapper";
+
+    const { isDialogTriggerClicked,  clickedElement: element} =
+        addEventBookNowTrigger(event, classWrapper);
 
     if (isDialogTriggerClicked) {
+        title = element.querySelector("h2").innerText;
+
+        if (element.querySelector("p")) {
+            mainContent = element.querySelector("p").innerText;
+        } else {
+            mainContent = `Current price: ${element.querySelector(".current-price").innerText}`;
+        }
+
+        modalTitle.innerText = title;
+        modalContent.innerText = mainContent;
+
         infoDialog.style.display = 'flex';
 
         // disable scroll
@@ -30,3 +51,22 @@ window.addEventListener('click', event => {
     }
 });
 
+function addEventBookNowTrigger(event, classWrapper) {
+    let clickedElement = null;
+    const isDialogTriggerClicked = [...infoDialogTriggers].some(
+        dialogTrigger => {
+            const result = dialogTrigger.contains(event.target);
+
+            if (result) {
+                clickedElement = event.target.closest(classWrapper)
+            }
+
+            return result
+        }
+    )
+
+    return {
+        isDialogTriggerClicked,
+        clickedElement
+    }
+}
